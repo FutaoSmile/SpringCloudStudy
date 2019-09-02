e47d50c3b33977d374d374c3e6f65499e756d977
futao <1185172056@qq.com>
2019-09-02 16:43:59 +0800

    logback

--- eureka-server/src/main/java/com/futao/eurekaserver/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/futao/eurekaserver/EurekaServerApplication.java
@@ -1,5 +1,9 @@
 package com.futao.eurekaserver;
 
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
@@ -10,12 +14,28 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
  * @author futao
  * Created on 2019/03/19
  */
+@Slf4j
 @EnableEurekaServer
 @SpringBootApplication
-public class EurekaServerApplication {
+public class EurekaServerApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(EurekaServerApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        log.trace("trace---");
+        log.debug("debug---");
+        log.info("info---");
+        log.warn("warn---");
+        log.error("error---");
+
+        Logger logger = LoggerFactory.getLogger("payment-log");
+        logger.trace("trace---payment");
+        logger.debug("debug---payment");
+        logger.info("info---payment");
+        logger.warn("warn---payment");
+        logger.error("error---payment");
+    }
 }
